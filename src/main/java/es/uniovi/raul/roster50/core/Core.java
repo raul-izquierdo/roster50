ac9bfee0d11e5fd160fad913205d3e1db622fcd0
raul-izquierdo <raul.izquierdo@gmail.com>
2026-09-19 17:14:47 +0200

    ui: improve message

--- src/main/java/es/uniovi/raul/roster50/core/Core.java
+++ src/main/java/es/uniovi/raul/roster50/core/Core.java
@@ -8,7 +8,7 @@ import es.uniovi.raul.roster50.roster.parser.RosterEntry;
 public class Core {
 
     public static final int OK = 0;
-    public static final int NO_CHANGES = 2; // The teams are already up to date, no changes were made
+    public static final int NO_CHANGES = 2; // The roster was up to date, no changes were made
     public static final int ERROR = 2;
 
     public static int run(List<RosterEntry> oldRoster, List<RosterEntry> newRoster, String newStudentsFile)
@@ -17,7 +17,7 @@ public class Core {
         var diff = RostersComparator.compareRosters(oldRoster, newRoster);
 
         if (!diff.hasChanges()) {
-            System.out.println("The teams are already up to date, no changes were made.");
+            System.out.println("The roster was up to date, no changes were made.");
             return NO_CHANGES;
         }
 
